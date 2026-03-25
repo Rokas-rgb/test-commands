9c8784fbb873d024cd270cedaef9d691d9585418
Rokas Bružas <79764461+Rokas-rgb@users.noreply.github.com>
2026-03-25 15:02:29 +0200

    Initial commit

---
+++ src/main/java/me/rokasrgb/testCommands/TestCommands.java
@@ -0,0 +1,17 @@
+package me.rokasrgb.testCommands;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TestCommands extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
