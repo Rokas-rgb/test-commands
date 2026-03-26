74cf2fb4230c5f63dd763e8cbe7f160244f416c4
Rokas Bružas <79764461+Rokas-rgb@users.noreply.github.com>
2026-03-26 09:30:26 +0200

    Added cooldown for feed command

--- src/main/java/me/rokasrgb/testCommands/TestCommands.java
+++ src/main/java/me/rokasrgb/testCommands/TestCommands.java
@@ -3,19 +3,28 @@ package me.rokasrgb.testCommands;
 import me.rokasrgb.testCommands.commands.FeedCmd;
 import me.rokasrgb.testCommands.commands.FlyCmd;
 import me.rokasrgb.testCommands.commands.HealCmd;
+import me.rokasrgb.testCommands.managers.FeedCooldown;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TestCommands extends JavaPlugin {
+
+    private FeedCooldown  feedCooldown;
 
     @Override
     public void onEnable() {
         this.getCommand("heal").setExecutor(new HealCmd());
         this.getCommand("feed").setExecutor(new FeedCmd());
         this.getCommand("fly").setExecutor(new FlyCmd());
+
+        feedCooldown = new FeedCooldown();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public FeedCooldown getFeedCooldown() {
+        return feedCooldown;
     }
 }
--- src/main/java/me/rokasrgb/testCommands/commands/FeedCmd.java
+++ src/main/java/me/rokasrgb/testCommands/commands/FeedCmd.java
@@ -1,5 +1,7 @@
 package me.rokasrgb.testCommands.commands;
 
+import me.rokasrgb.testCommands.TestCommands;
+import me.rokasrgb.testCommands.managers.FeedCooldown;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -7,6 +9,12 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class FeedCmd implements CommandExecutor {
+
+    private TestCommands plugin;
+
+    public FeedCmd(TestCommands plugin) {
+        this.plugin = plugin;
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -23,8 +31,17 @@ public class FeedCmd implements CommandExecutor {
                 return true;
             }
 
+            FeedCooldown feedCooldown = plugin.getFeedCooldown();
+
+            if(feedCooldown.onCooldown(player, 3600)) {
+             player.sendMessage(ChatColor.RED + "You are on cooldown!: " + feedCooldown.getRemaining(player, 3600));
+             return true;
+            }
+
             player.setFoodLevel(20);
             player.sendMessage(ChatColor.GREEN + "You have now eaten!");
+
+            feedCooldown.setCooldown(player);
 
             return true;
         }
