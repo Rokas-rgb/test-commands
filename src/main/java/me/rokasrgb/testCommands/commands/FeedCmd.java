271689fcf52314f23e357fa754c12d109b90a277
Rokas Bružas <79764461+Rokas-rgb@users.noreply.github.com>
2026-04-09 15:09:42 +0300

    Finished /feed command

--- src/main/java/me/rokasrgb/testCommands/commands/FeedCmd.java
+++ src/main/java/me/rokasrgb/testCommands/commands/FeedCmd.java
@@ -2,6 +2,7 @@ package me.rokasrgb.testCommands.commands;
 
 import me.rokasrgb.testCommands.TestCommands;
 import me.rokasrgb.testCommands.managers.FeedCooldown;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -26,24 +27,37 @@ public class FeedCmd implements CommandExecutor {
 
         if (command.getName().equalsIgnoreCase("feed")) {
 
-            if(!player.hasPermission("permissions.feed")) {
-                player.sendMessage(ChatColor.RED + "You do not have permissions to execute this command!");
+            if (args.length <1) {
+                if (!player.hasPermission("permissions.feed")) {
+                    player.sendMessage(ChatColor.RED + "You do not have permissions to execute this command!");
+                    return true;
+                }
+
+                FeedCooldown feedCooldown = plugin.getFeedCooldown();
+
+                if (feedCooldown.onCooldown(player, 3600)) {
+                    player.sendMessage(ChatColor.RED + "You are on cooldown!: " + feedCooldown.getRemaining(player, 3600));
+                    return true;
+                }
+
+                player.setFoodLevel(20);
+                player.sendMessage(ChatColor.GREEN + "You have now eaten!");
+
+                feedCooldown.setCooldown(player);
+
                 return true;
+            } else{
+                String playerName  = args[0];
+
+                Player target = Bukkit.getServer().getPlayerExact(playerName);
+
+                if(target == null) {
+                    player.sendMessage(ChatColor.RED + "Player not found!");
+                } else{
+                    target.setFoodLevel(20);
+                    target.sendMessage(ChatColor.GREEN + "You have been fed by" + player + "!");
+                }
             }
-
-            FeedCooldown feedCooldown = plugin.getFeedCooldown();
-
-            if(feedCooldown.onCooldown(player, 3600)) {
-             player.sendMessage(ChatColor.RED + "You are on cooldown!: " + feedCooldown.getRemaining(player, 3600));
-             return true;
-            }
-
-            player.setFoodLevel(20);
-            player.sendMessage(ChatColor.GREEN + "You have now eaten!");
-
-            feedCooldown.setCooldown(player);
-
-            return true;
         }
         return true;
     }
